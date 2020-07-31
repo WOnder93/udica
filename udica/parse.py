f17a165087919083ac2ef39b1b1c0099d2c9f301
Ondrej Mosnacek <omosnace@redhat.com>
2020-07-31 22:05:03 +0200

    DEBUG

--- udica/parse.py
+++ udica/parse.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import json
+import json, sys
 
 #: Constant for the podman engine
 ENGINE_PODMAN = "podman"
@@ -99,6 +99,8 @@ def get_mounts(data, inspect_format):
 def get_ports(data, inspect_format):
     if inspect_format in [ENGINE_PODMAN, ENGINE_DOCKER]:
         ports = []
+        print(data[0]["NetworkSettings"]["Ports"], file=sys.stderr)
+        print(type(data[0]["NetworkSettings"]["Ports"]), file=sys.stderr)
         for key, value in data[0]["NetworkSettings"]["Ports"].items():
             container_port = str(key).split("/")
             host_port = value[0]["HostPort"]
